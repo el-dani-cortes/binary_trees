7eb2850c848c1e8bbd385c267cd0fb05a537373a
el-dani-cortes <danicor83@gmail.com>
2021-03-10 15:02:41 +0000

    Changed the position of the condition to validate if the malloc fails

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,26 +13,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (parent == NULL || new_node == NULL)
-	{
+	if (parent == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
 	else
 	{
-		if (parent->left != NULL)
-		{
-			new_node->left = parent->left;
-			parent->left->parent = new_node;
-		}
-		else
-		{
-			new_node->left = NULL;
-		}
-		new_node->parent = parent;
-		parent->left = new_node;
-		new_node->n = value;
-		new_node->right = NULL;
+		new_node->left = NULL;
 	}
+	new_node->parent = parent;
+	parent->left = new_node;
+	new_node->n = value;
+	new_node->right = NULL;
 	return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,28 +14,23 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (parent == NULL || new_node == NULL)
-	{
+	if (parent == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
 	else
 	{
-		if (parent->right != NULL)
-		{
-			new_node->right = parent->right;
-			parent->right->parent = new_node;
-		}
-		else
-		{
-			new_node->right = NULL;
-
-		}
-		new_node->parent = parent;
-		new_node->n = value;
-		parent->right = new_node;
-		new_node->left = NULL;
-
+		new_node->right = NULL;
 	}
+	new_node->parent = parent;
+	new_node->n = value;
+	parent->right = new_node;
+	new_node->left = NULL;
 	return (new_node);
 }
