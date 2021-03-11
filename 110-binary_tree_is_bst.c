d08f45c0d100c7dbb6dde3444a756f63feb10705
7honarias <7honarias@gmail.com>
2021-03-11 12:35:33 +0000

    [UPDATE] - exercise checks if a binary tree is a valid bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+/**
+ * maxValue - find the max value of a bianry tree
+ * @tree: is a pointer to the root node
+ * Return: max value
+ */
 
 int maxValue(const binary_tree_t *tree)
 {
@@ -22,8 +27,9 @@ int maxValue(const binary_tree_t *tree)
 	return (res);
 }
 /**
- *
- *
+ * minValue - find the min value of a binary tree
+ * @tree: is a pointer to the root node
+ * Return: min value
  */
 int minValue(const binary_tree_t *tree)
 {
@@ -42,14 +48,16 @@ int minValue(const binary_tree_t *tree)
 }
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_is_bst - checks if a binary tree is a valid bst
+ * @tree: is a pointer to the roo node
+ * Return: 1 if tree is valid bst, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	int l_res, r_res;
+
 	if (tree == NULL)
-		return(1);
+		return (0);
 	if (tree->left != NULL && maxValue(tree->left) > tree->n)
 	{
 		return (0);
@@ -58,7 +66,11 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
+	if (tree->left)
+		l_res = binary_tree_is_bst(tree->left);
+	if (tree->right)
+		r_res = binary_tree_is_bst(tree->right);
+	if (!l_res || !r_res)
 		return (0);
 	return (1);
 
