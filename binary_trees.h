80552439f90719d69fecf1f6867f79a4f18afa30
7honarias <7honarias@gmail.com>
2021-03-11 22:24:43 +0000

    [UPDATE] - file header with new functions

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,5 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-
 #include <stddef.h>
 
 /**
@@ -11,36 +10,25 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+        int n;
+        struct binary_tree_s *parent;
+        struct binary_tree_s *left;
+        struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
-
-/*Fution that prints a binary tree*/
 void binary_tree_print(const binary_tree_t *);
-
-/*Function that creates a new node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-/*Function that insert a node to the left of the parent*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
-/*Function that insert a node to the right of the parent*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-/*Function that deletes all the binary tree structure*/
 void binary_tree_delete(binary_tree_t *tree);
-
-/*Function that checks if a node is a leaf*/
 int binary_tree_is_leaf(const binary_tree_t *node);
-
-/*Function that checks if a node is the root*/
 int binary_tree_is_root(const binary_tree_t *node);
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
@@ -60,5 +48,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* _BINARY_TREES_H_ */
