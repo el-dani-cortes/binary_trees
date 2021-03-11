e7a6323fddaa4b5ccfa874d8b00cb34d9c0df7af
el-dani-cortes <danicor83@gmail.com>
2021-03-11 01:44:59 +0000

    Added a function that rotates to the left the binary tree

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Performs a left-rotation on a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *temp = NULL;
+
+	temp = tree->right;
+	if (tree->right != NULL)
+	{
+		temp->parent = tree->parent;
+		if (temp->left)
+		{
+			tree->right = temp->left;
+			temp->left->parent = tree;
+		}
+		else
+			tree->right = NULL;
+		temp->left = tree;
+		tree->parent = temp;
+	}
+	return (temp);
+}
