c4a4c3075448a8675b75d8c3f5790628b60f5e5b
7honarias <7honarias@gmail.com>
2021-03-12 02:09:03 +0000

    [ADD] - correction checks if a binary tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -3,54 +3,22 @@
 #include "binary_trees.h"
 /**
  * maxValue - find the max value of a bianry tree
+ * @greater: num more big
+ * @lesser: num less
  * @tree: is a pointer to the root node
  * Return: max value
  */
 
-int maxValue(const binary_tree_t *tree)
+int maxValue(const binary_tree_t *tree, int lesser, int greater)
 {
-	int res;
-	int lres;
-	int rres;
-
-	res = tree->n;
-	lres = tree->n;
-	rres = tree->n;
-
-	if (tree->left)
-		lres = maxValue(tree->left);
-	if (tree->right)
-		rres = maxValue(tree->right);
-
-	if (lres > res)
-		res = lres;
-	if (rres > res)
-		res = rres;
-
-	return (res);
+	if (tree == NULL)
+		return (1);
+	if (tree->n >= greater || tree->n <= lesser)
+		return (0);
+	return ((maxValue(tree->left, lesser, tree->n)) &&
+		(maxValue(tree->right, tree->n, greater)));
 }
-/**
- * minValue - find the min value of a binary tree
- * @tree: is a pointer to the root node
- * Return: min value
- */
-int minValue(const binary_tree_t *tree)
-{
-	int rest, lres, rres;
 
-	rest = tree->n;
-	lres = tree->n;
-	rres = tree->n;
-	if (tree->left)
-		lres = minValue(tree->left);
-	if (tree->right)
-		rres = minValue(tree->right);
-	if (lres < rest)
-		rest = lres;
-	if (rres < rest)
-		rest = rres;
-	return (rest);
-}
 
 /**
  * binary_tree_is_bst - checks if a binary tree is a valid bst
@@ -59,24 +27,8 @@ int minValue(const binary_tree_t *tree)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int l_res, r_res;
-
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL && maxValue(tree->left) >= tree->n)
-	{
-		return (0);
-	}
-	if (tree->right != NULL && minValue(tree->right) <= tree->n)
-	{
-		return (0);
-	}
-	if (tree->left)
-		l_res = binary_tree_is_bst(tree->left);
-	if (tree->right)
-		r_res = binary_tree_is_bst(tree->right);
-	if (!l_res || !r_res)
-		return (0);
-	return (1);
+	return (maxValue(tree, INT_MIN, INT_MAX));
 
 }
